8e4e990d26583ec51f154fe1724d8af5ea607317
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-08-20 22:13:18 -0700

    Modify error in string

--- edit_sudoers.py
+++ edit_sudoers.py
@@ -1,6 +1,6 @@
 with open('/etc/sudoers', 'r') as file:
 	data = file.readlines()
-data[82] = "%wheel ALL=(ALL) ALL"
+data[82] = "%wheel ALL=(ALL) ALL\n"
 
 with open('/etc/sudoers', 'w') as file:
 	file.writelines(data)
